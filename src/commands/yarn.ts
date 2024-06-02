7d568ddffb80fcb1384b4ffc2d5f2ac4c1f01faa
mrmekstuff <olanzovc@gmail.com>
2024-06-02 19:08:17 -0400

    feat: added yarn command

---
+++ src/commands/yarn.ts
@@ -0,0 +1,23 @@
+import fs from 'fs'
+import {Args, Command, Flags} from '@oclif/core'
+import {execSync} from 'child_process'
+import path from 'path'
+
+export default class Yarn extends Command {
+  static description = 'describe the command here'
+
+  public async run(): Promise<void> {
+    const yarnIntegrityPath = path.join(process.cwd(), 'node_modules', '.yarn-integrity')
+    if (fs.existsSync(yarnIntegrityPath)) {
+      this.log(`Removing yarn integrity`)
+      fs.rmSync(yarnIntegrityPath)
+    }
+    const commandToExecute = 'yarn install'
+    this.log(`Running "${commandToExecute}"`)
+    try {
+      execSync(commandToExecute)
+    } catch (err) {
+      this.warn(`Failed to execute "${commandToExecute}" successfully. => ${err}`)
+    }
+  }
+}
